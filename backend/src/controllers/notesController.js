ba48e09c022fa3f118b299770d3e361c07c93df9
thelouisgram <hadesanoye01@gmail.com>
2025-09-16 06:39:00 +0100

    new new

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(_, res) {
   try {
-    const notes = await Note.find().sort({ createdAt: -1 }); // -1 will sort in desc. order (newest first)
+    const notes = await Note.find().sort({ createdAt: -1 }); 
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error in getAllNotes controller", error);
